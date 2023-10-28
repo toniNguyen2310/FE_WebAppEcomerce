fe3e1f8475da6e30653278d4fe4d9425ce99f5e6
Toni Nguyen <xuantunguyen2310@gmail.com>
2023-10-28 13:04:01 +0700

    update 2nd design & handle submenu

--- src/Components/CartHoverHeader/CartHoverHeader.jsx
+++ src/Components/CartHoverHeader/CartHoverHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./CartHoverHeader.scss";
+import "./cartHoverHeader.scss";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { convertSlug } from "../Homepage";
